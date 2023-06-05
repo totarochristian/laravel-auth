0dc492bfa3aecb1f41dcaa1dcd4fbc16ed04ab3b
totarochristian <totaro.christian.18@gmail.com>
2023-06-05 12:35:43 +0200

    Add Admin template initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,11 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+const btnToggleSidebar = document.getElementById('sidebarToggle');
+btnToggleSidebar.addEventListener('click', (e) => {
+    const bodyEl = document.querySelector('body');
+    bodyEl.classList.toggle('sidebar-toggled');
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('toggled');
+});
