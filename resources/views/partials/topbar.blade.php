bea5a424de715a9cd8b5ae2c6676bed325593f3b
totarochristian <totaro.christian.18@gmail.com>
2023-06-05 16:07:17 +0200

    Fix topbar profile redirect

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -171,7 +171,7 @@
 
             <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="{{ url('/admin') }}">{{ __('Dashboard') }}</a>
-                <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                <a class="dropdown-item" href="{{ route('admin.profile.edit') }}">{{ __('Profile') }}</a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
