dab9a3a503dda36de119e718a8c7ccc3641f1c47
totarochristian <totaro.christian.18@gmail.com>
2023-06-06 21:20:07 +0200

    Define the seeder of projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,6 +16,14 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $projects = config('projectsData.projects');
+        foreach ($projects as $project) {
+            $newProj = new Project();
+            $newProj->title = $project['title'];
+            $newProj->slug = Str::slug($project['title'], '-');
+            $newProj->image = $project['image'];
+            $newProj->body = $project['body'];
+            $newProj->save();
+        }
     }
 }
