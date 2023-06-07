2a2033d3946f7cc71da0d7404683a1bbc106662f
totarochristian <totaro.christian.18@gmail.com>
2023-06-07 11:06:44 +0200

    Update project foreign links

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,7 +18,8 @@ class Project extends Model
      * Define the relation with users elements.
      * One project belongs to an user.
      */
-    public function users(){
+    public function users(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 }
